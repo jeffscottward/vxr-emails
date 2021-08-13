b92deffeaa0a74c965ee06d71806c0d738730e84
Jeff Ward <jsward.17@gmail.com>
2021-08-13 18:49:29 -0400

    build and inline

---
+++ out/_next/static/v1kk66je_CPpP_a41V8aP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/157-fb7a5d052fccc7e993b6.js","static/chunks/pages/index-cff00bc6d5047385b680.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
